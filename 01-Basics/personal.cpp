d002d3d8dff39df45e532c264c96d1a2a3a78b2f
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-09-05 01:40:03 +0530

    added practice

---
+++ 01-Basics/personal.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string name;
+    cout<<"Enter your name: ";
+    getline(cin,name);
+    cout<<"Enter your age: ";
+    int age;
+    cin>> age;
+    cout<<"Enter your location: ";
+    string loc;
+    cin>> loc;
+    
+    cout<<"I know you are " <<name<< " and your age is " <<age <<" rAlso you are from "<<loc;
+    return 0;
+}
