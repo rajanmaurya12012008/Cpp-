193cfa1d95078a067d6c1ea38d3a6cb71ac18271
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-09-08 02:07:41 +0530

    added practice

--- 01-Basics/sum.cpp
+++ 01-Basics/sum.cpp
@@ -1,13 +1,11 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int a;
-    int b;
+    int a, b;
     cout<<"Enter first no.: ";
     cin>>a;
     cout<<"Enter second no.: ";
     cin>>b;
-    int sum;
-    sum = a+b;
+    int sum = a+b;
     cout<<"Sum of two no. is: "<<sum;
 }
