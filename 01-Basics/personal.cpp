310c44f3c9eddbf6106bfbced6942df46da23c26
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-09-22 01:19:26 +0530

    added practice

--- 01-Basics/personal.cpp
+++ 01-Basics/personal.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main() {
     string name;
-    cout<<"Enter Your name: ";
+    cout<<"Enter your name: ";
     getline(cin,name);
     cout<<"Enter your age: ";
     int age;
     cin>> age;
-    cout<<"Enter Your location: ";
+    cout<<"Enter your location: ";
     string loc;
     cin>> loc;
     cout<<"I know you are " <<name<< " and your age is " <<age <<" Also you are from "<<loc;
